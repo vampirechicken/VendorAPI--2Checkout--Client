3865edd66977e60224526d57ce565bf8202ee699
Len Jaffe <lenjaffe@jaffesystems.com>
2011-07-04 19:26:49 -0400

    - bump version to 0.06 after merging the sort_params TB

--- lib/VendorAPI/2Checkout/Client.pm
+++ lib/VendorAPI/2Checkout/Client.pm
@@ -16,11 +16,11 @@ VendorAPI::2Checkout::Client - an OO interface to the 2Checkout.com Vendor API
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use constant {
      VAPI_BASE_URI => 'https://www.2checkout.com/api/sales',
