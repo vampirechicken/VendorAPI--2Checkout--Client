fc9a434218b2c607aae9ec5308bdda23bacc84d0
Len Jaffe <lenjaffe@jaffesystems.com>
2011-07-17 17:03:04 -0400

    -forgot to update the README for VAPI_HAS_COUPONS

--- lib/VendorAPI/2Checkout/Client.pm
+++ lib/VendorAPI/2Checkout/Client.pm
@@ -18,11 +18,11 @@ VendorAPI::2Checkout::Client - an OO interface to the 2Checkout.com Vendor API
 
 =head1 VERSION
 
-Version 0.0900
+Version 0.0901
 
 =cut
 
-our $VERSION = '0.0900';
+our $VERSION = '0.0901';
 
 use constant {
      VAPI_BASE_URI => 'https://www.2checkout.com/api',
