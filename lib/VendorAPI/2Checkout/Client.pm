baa83daafb72e7b5305d120e361f2844988f2174
Len Jaffe <lenjaffe@jaffesystems.com>
2012-01-09 12:47:49 -0500

    - refactored the non-moose guts into lin/VendorAPI/2Checkout/Client/NoMoose.pm so that the old   constructor can act as a factory for moose and non-moose.

--- lib/VendorAPI/2Checkout/Client.pm
+++ lib/VendorAPI/2Checkout/Client.pm
@@ -10,23 +10,41 @@ use Carp qw(confess);
 use URI;
 use URI::QueryParam;
 
+use VendorAPI::2Checkout::Client::NoMoose;
+
+require Exporter;
+
+
 =head1 NAME
 
 VendorAPI::2Checkout::Client - an OO interface to the 2Checkout.com Vendor API
 
 =head1 VERSION
 
-Version 0.1100
+Version 0.1300
 
 =cut
 
-our $VERSION = '0.1200';
+use vars qw( $VERSION @ISA @EXPORT_OK %EXPORT_TAGS ); 
+$VERSION = '0.1300';
+
+
+sub _base_uri { 'https://www.2checkout.com/api' };
+sub _realm    { '2CO API' };
+sub _netloc   { 'www.2checkout.com:443' };
 
 use constant {
-     VAPI_BASE_URI => 'https://www.2checkout.com/api',
-     VAPI_REALM    => '2CO API',
-     VAPI_NETLOC   => 'www.2checkout.com:443',
+     VAPI_MOOSE    => 1,
+     VAPI_NO_MOOSE => 0,
 };
+
+@ISA = qw( Exporter );
+%EXPORT_TAGS = ( ':constants' => [ qw( 
+                                       VAPI_MOOSE VAPI_NO_MOOSE
+                                     )
+                                 ]
+               );
+@EXPORT_OK = qw( @{ $EXPORT_TAGS{':constants'} } );
 
 =head1 SYNOPSIS
 
@@ -68,7 +86,9 @@ for input parameters and expexted return values.
 
 =over 4
 
-=item $c = VendorAPI::2Checkout::Client->new($username, $password, $format)
+=item $c = VendorAPI::2Checkout::Client->new($username, $password, $format, VAPI_MOOSE)
+=item $c = VendorAPI::2Checkout::Client->new($username, $password, $format, VAPI_NO_MOOSE)
+=item $c = VendorAPI::2Checkout::Client->new($username, $password, $format) # no moose
 
 Contructs a new C<VendorAPI::2Checkout::Client> object to comminuncate with the 2Checkout Back Office Admin API.
 You must pass your Vendor API username and password or the constructor will return undef;
@@ -78,10 +98,11 @@ You must pass your Vendor API username and password or the constructor will retu
 my %accept_mime_types = ( XML => 'application/xml', JSON => 'application/json', );
 
 sub new {
-   my $class = shift;
-   my $username = shift;
-   my $password = shift;
-   my $accept   = shift;
+   my $class     = shift;
+   my $username  = shift;
+   my $password  = shift;
+   my $accept    = shift;
+   my $use_moose = shift;
 
    unless ( $username && $password) {
       return undef;
@@ -91,175 +112,15 @@ sub new {
       $accept = 'XML';
    }
 
-   my $ua = LWP::UserAgent->new( agent => "VendorAPI::2Checkout::Client/${VERSION} " );
-   $ua->credentials(VAPI_NETLOC, VAPI_REALM, $username, $password);
-   return bless { ua => $ua, accept => $accept_mime_types{$accept}  }, $class;
-}
-
-=item $response = $c->list_sales();
-
-Retrieves the list of sales for the vendor
-
-=cut
-
-my $sort_col_re = qr/^(sale_id|date_placed|customer_name|recurring|recurring_declined|usd_total)$/;
-my $sort_dir_re = qr/^(ASC|DESC)$/;
-
-my %v = (
-             sale_id             => { type => SCALAR, regex => qw/^\d+$/ , untaint => 1, optional => 1, },
-             invoice_id          => { type => SCALAR, regex => qw/^\d+$/ , untaint => 1, optional => 1, },
-             pagesize            => { type => SCALAR, regex => qw/^\d+$/ , untaint => 1, optional => 1, },
-             cur_page            => { type => SCALAR, regex => qw/^\d+$/ , untaint => 1, optional => 1, },
-             customer_name       => { type => SCALAR, regex => qw/^[-A-Za-z.]+$/ , untaint => 1, optional => 1, },
-             customer_email      => { type => SCALAR, regex => qw/^[-\w.+@]+$/ , untaint => 1, optional => 1,   },
-             customer_phone      => { type => SCALAR, regex => qw/^[\d()-]+$/ , untaint => 1, optional => 1,    },
-             vendor_product_id   => { type => SCALAR, regex => qw/^.+$/ , untaint => 1, optional => 1,    },
-             ccard_first6        => { type => SCALAR, regex => qw/^\d{6}$/ , untaint => 1, optional => 1, },
-             ccard_last2         => { type => SCALAR, regex => qw/^\d\d$/ , untaint => 1, optional => 1,  },
-             date_sale_end       => { type => SCALAR, regex => qw/^\d{4}-\d\d-\d\d$/ , untaint => 1, optional => 1, },
-             date_sale_begin     => { type => SCALAR, regex => qw/^\d{4}-\d\d-\d\d$/ , untaint => 1, optional => 1, },
-             sort_col            => { type => SCALAR, regex => $sort_col_re  , untaint => 1, optional => 1, },
-             sort_dir            => { type => SCALAR, regex => $sort_dir_re , untaint => 1, optional => 1,  },
-             active_recurrings   => { type => SCALAR, regex => qr/^[01]$/, untaint => 1, optional => 1, },
-             declined_recurrings => { type => SCALAR, regex => qr/^[01]$/, untaint => 1, optional => 1, },
-             refunded            => { type => SCALAR, regex => qr/^[01]$/, untaint => 1, optional => 1, },
-        );
-
-my $_profile = { map { $_ => $v{$_} } keys %v };
-
-sub list_sales {
-   my $self = shift;
-   my $uri = URI->new(VAPI_BASE_URI . '/sales/list_sales');
-   my %headers = ( Accept => $self->_accept() );
-
-   my %input_params = validate(@_, $_profile);
-
-   foreach my $param_name ( keys %input_params ) {
-      $uri->query_param($param_name => $input_params{$param_name});
+   if (defined $use_moose && $use_moose) {
+      die 'Moose not implemented yet'
    }
 
-   $self->_ua->get($uri, %headers);
-}
-
-=item $response = $c->list_coupons();
-
-Retrieves the list of coupons for the vendor
-
-=cut
-
-sub list_coupons {
-   my $self = shift;
-   my $uri = URI->new(VAPI_BASE_URI . '/products/list_coupons');
-   my %headers = ( Accept => $self->_accept() );
-   $self->_ua->get($uri, %headers);
-}
-
-=item  $response = $c->detail_sale(sale_id => $sale_id);
-
-Retrieves the details for the named sale.
-
-=cut
-
-sub detail_sale {
-   my $self = shift;
-
-   my $_detail_profile = { map { $_ => $v{$_} } qw/sale_id invoice_id/ };
-   my %p = validate(@_, $_detail_profile);
-
-   unless ($p{sale_id} || $p{invoice_id}) {
-      confess("detail_sale requires sale_id or invoice_id and received neither");
-   }
-
-   my $uri = URI->new(VAPI_BASE_URI . '/sales/detail_sale');
-   my %headers = ( Accept => $self->_accept() );
-
-   if ($p{invoice_id} ) {
-      $uri->query_param(invoice_id => $p{invoice_id});
-   }
-   else {
-      $uri->query_param(sale_id => $p{sale_id});
-   }
-
-   $self->_ua->get($uri, %headers);
-}
-
-=item  $response = $c->detail_coupon(coupon_code => $coupon_code);
-
-Retrieves the details for the named coupon.
-
-=cut
-
-sub detail_coupon {
-   my $self = shift;
-   my $_detail_profile = {
-             coupon_code => { type => SCALAR, regex => qr/^\w+$/, untaint => 1, optional => 0, },
-                         };
-
-   my %p = validate(@_, $_detail_profile);
-
-   unless ( $p{coupon_code} ) {
-      confess("detail_coupon requires coupon_code");
-   }
-
-   my $uri = URI->new(VAPI_BASE_URI . '/products/detail_coupon');
-   my %headers = ( Accept => $self->_accept() );
-
-   $uri->query_param(coupon_code => $p{coupon_code});
-
-   $self->_ua->get($uri, %headers);
-}
-
-=item $response = $c->list_payments();
-
-Retrieves the list of payments for the vendor
-
-=cut
-
-sub list_payments {
-   my $self = shift;
-   my $uri = URI->new(VAPI_BASE_URI . '/acct/list_payments');
-   my %headers = ( Accept => $self->_accept() );
-   $self->_ua->get($uri, %headers);
-}
-
-=item $response = $c->list_products();
-
-Retrieves the list of products for the vendor
-
-=cut
-
-sub list_products {
-   my $self = shift;
-   my $uri = URI->new(VAPI_BASE_URI . '/products/list_products');
-   my %headers = ( Accept => $self->_accept() );
-   $self->_ua->get($uri, %headers);
-}
-
-=item $response = $c->list_options();
-
-Retrieves the list of options for the vendor
-
-=cut
-
-sub list_options {
-   my $self = shift;
-   my $uri = URI->new(VAPI_BASE_URI . '/products/list_options');
-   my %headers = ( Accept => $self->_accept() );
-   $self->_ua->get($uri, %headers);
+   return VendorAPI::2Checkout::Client::NoMoose->new($username, $password, $accept);
 }
 
 
-#####################################################
 
-sub _accept {
-   $_[0]->{accept};
-};
-
-sub _ua {
-   $_[0]->{ua};
-};
-
-=back
 
 =head1 AUTHOR
 
@@ -308,7 +169,7 @@ L<http://search.cpan.org/dist/VendorAPI-2Checkout-Client/>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2011 Len Jaffe.
+Copyright 2012 Len Jaffe.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
